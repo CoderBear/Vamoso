059dc5ec529e64be5c21c1c5580fbcf4e3d424b6
Joe Ortega <joeortega79@gmail.com>
2018-07-17 12:03:00 +0930

    0.001.005
    started with playerInput Class

---
+++ Vamoso/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	public float h;
+	public float v;
+	public bool inputEnabled = false;
+	// Use this for initialization
+
+	public void GetKeyInput()
+	{
+		if (inputEnabled) {
+			h = Input.GetAxisRaw ("Horizontal");
+			v = Input.GetAxisRaw ("Vertical");
+		}
+	}
+}
