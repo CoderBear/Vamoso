9d757cb2d950ef0cfac1f7984af3b2d3dd51f7ac
Joe Ortega <joeortega79@gmail.com>
2018-07-19 15:44:04 +0930

    0.003.005
    Finished Neighbor nodes code

--- Vamoso/Assets/Scripts/Node.cs
+++ Vamoso/Assets/Scripts/Node.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+	Vector2 m_coordinate;
+	public Vector2 Coordinate { get { return Utility.Vector2Round(m_coordinate); } }
+
+	List<Node> m_neighborNodes = new List<Node> ();
+	public List<Node> NeighborNodes { get { return m_neighborNodes; } }
+
+	Board m_board;
+
 	public GameObject geometry;
 	public float scaleTime = 0.3f;
 	public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
@@ -11,6 +19,13 @@ public class Node : MonoBehaviour
 	public bool autoRun = false;
 
 	public float delay = 1f;
+
+	void Awake()
+	{
+		m_board = Object.FindObjectOfType<Board> ();
+		m_coordinate = new Vector2 (transform.position.x, transform.position.z);
+	}
+
 	// Use this for initialization
 	void Start () {
 		if (geometry != null) {
@@ -18,6 +33,11 @@ public class Node : MonoBehaviour
 
 			if (autoRun) {
 				ShowGeometry ();
+			}
+
+			if(m_board != null)
+			{
+				m_neighborNodes = FindNeighbors (m_board.AllNodes);
 			}
 		}
 	}
@@ -32,5 +52,21 @@ public class Node : MonoBehaviour
 				"delay", delay
 			));
 		}
+	}
+
+	public List<Node> FindNeighbors(List<Node> nodes)
+	{
+		List<Node> nList = new List<Node> ();
+
+		foreach(Vector2 dir in Board.directions)
+		{
+			Node foundNeighbor = nodes.Find (n => n.Coordinate == m_coordinate + dir);
+
+			if(foundNeighbor != null && !nList.Contains (foundNeighbor))
+			{
+				nList.Add (foundNeighbor);
+			}
+		}
+		return nList;
 	}
 }
